4e564caf02eed16e1c53907f8f5b85e3be0ff09e
Isaac H <undermybrella@abimon.org>
2020-05-19 17:53:12 +1000

    Undelete KProperty<T>.getValue

--- io/src/jvmMain/kotlin/org/abimon/kornea/io/jvm/PropertyUtils.kt
+++ io/src/jvmMain/kotlin/org/abimon/kornea/io/jvm/PropertyUtils.kt
@@ -1,1 +1,6 @@
 package org.abimon.kornea.io.jvm
+
+import kotlin.reflect.KProperty
+
+@Deprecated("This will be natively supported in 1.4-M2")
+operator fun <T> KProperty<T>.getValue(thisRef: Any, property: KProperty<*>): T = this.call()
