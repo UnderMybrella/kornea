b3d078b2d259caaf133fa1ad3ca7ab0e148f3b2b
Isaac H <undermybrella@abimon.org>
2020-05-22 17:22:07 +1000

    Fix position (again)

--- io/src/jvmMain/kotlin/org/abimon/kornea/io/jvm/files/AsyncFileInputFlow.kt
+++ io/src/jvmMain/kotlin/org/abimon/kornea/io/jvm/files/AsyncFileInputFlow.kt
@@ -210,7 +210,7 @@ class AsyncFileInputFlow(
     override suspend fun remaining(): ULong = size() - position()
     override suspend fun size(): ULong = withContext(Dispatchers.IO) { channel.size().toULong() }
     override suspend fun position(): ULong =
-        withContext(Dispatchers.IO) { bufferMutex.withLock { flowFilePointer - buffer.position() } }.toULong()
+        withContext(Dispatchers.IO) { bufferMutex.withLock { flowFilePointer - buffer.limit() + buffer.position() } }.toULong()
 
     override suspend fun seek(pos: Long, mode: EnumSeekMode): ULong {
         bufferMutex.withLock {
