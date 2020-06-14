f426abd62d80a76c2e08231e2eba64b2a07ccdde
Isaac H <undermybrella@abimon.org>
2020-06-14 20:49:46 +1000

    Update Experimental annotations to give a message

--- annotations/src/commonMain/kotlin/org/abimon/kornea/annotations/ExperimentalKorneaErrors.kt
+++ annotations/src/commonMain/kotlin/org/abimon/kornea/annotations/ExperimentalKorneaErrors.kt
@@ -2,4 +2,5 @@ package org.abimon.kornea.annotations
 
 @Target(AnnotationTarget.CLASS, AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.TYPE_PARAMETER, AnnotationTarget.PROPERTY, AnnotationTarget.FIELD, AnnotationTarget.LOCAL_VARIABLE, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.CONSTRUCTOR, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER, AnnotationTarget.TYPE)
 @RequiresOptIn(level = RequiresOptIn.Level.WARNING)
-public annotation class ExperimentalKorneaErrors
+@ChangedSince(KorneaAnnotations.VERSION_1_3_0)
+public annotation class ExperimentalKorneaErrors(val message: String = "")
