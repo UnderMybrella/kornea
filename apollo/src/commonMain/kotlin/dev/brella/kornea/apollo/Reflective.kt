e40d3dd7a67c0a20e6ba708ad7cace0a33b6875f
Isaac H <undermybrella@abimon.org>
2020-06-28 18:13:16 +1000

    Remove common module for kornea-apollo

--- apollo/src/commonMain/kotlin/dev/brella/kornea/apollo/Reflective.kt
+++
@@ -1,6 +0,0 @@
-package dev.brella.kornea.apollo
-
-
-@Target(AnnotationTarget.PROPERTY, AnnotationTarget.FIELD, AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
-public annotation class Reflective
