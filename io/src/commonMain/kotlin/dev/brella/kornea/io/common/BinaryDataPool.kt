0639278b5abfd38c6c9e1367ffe41d3178bc1587
Izzy <undermybrella@abimon.org>
2020-08-20 09:11:28 +1000

    Fix a bizarre bug with function selection and common source sets

--- io/src/commonMain/kotlin/dev/brella/kornea/io/common/BinaryDataPool.kt
+++ io/src/commonMain/kotlin/dev/brella/kornea/io/common/BinaryDataPool.kt
@@ -9,12 +9,25 @@ import dev.brella.kornea.toolkit.common.ObservableDataCloseable
 
 @ExperimentalKorneaToolkit
 @ExperimentalUnsignedTypes
-public expect class BinaryDataPool: DataPool<BinaryInputFlow, OutputFlowByDelegate<BinaryOutputFlow>>, LimitedInstanceDataSource.Typed<BinaryInputFlow, BinaryDataPool> {
+public expect class BinaryDataPool : DataPool<BinaryInputFlow, OutputFlowByDelegate<BinaryOutputFlow>>,
+    LimitedInstanceDataSource.Typed<BinaryInputFlow, BinaryDataPool> {
     public companion object {
-        public suspend operator fun invoke(
-            location: String? = null,
-            maximumInstanceCount: Int? = null,
-            maximumPermitCount: Int = 8
+        @PublishedApi
+        /**
+         * "Why do we declare invoke as an expected function *and* as a extension?"
+         *
+         * Excellent question!
+         *
+         * The problem is, in scenarios where the common source set is *also* able to reference coroutineMain,
+         * it seems to prioritise the actual declaration *not* the expect declaration, and doesn't recognise the defaults
+         *
+         * This is a pain in the ass
+         *
+         */
+        internal suspend operator fun invoke(
+            location: String?,
+            maximumInstanceCount: Int?,
+            maximumPermitCount: Int
         ): BinaryDataPool
     }
 
@@ -23,8 +36,16 @@ public expect class BinaryDataPool: DataPool<BinaryInputFlow, OutputFlowByDelega
     internal var outputClosed: Boolean
 
     override suspend fun openNamedInputFlow(location: String?): KorneaResult<BinaryInputFlow>
+
     @Suppress("RedundantSuspendModifier")
     internal suspend fun onOutputClosed(closeable: ObservableDataCloseable)
     override suspend fun openOutputFlow(): KorneaResult<OutputFlowByDelegate<BinaryOutputFlow>>
     override suspend fun canOpenOutputFlow(): Boolean
 }
+
+@ExperimentalKorneaToolkit
+public suspend inline operator fun BinaryDataPool.Companion.invoke(
+    location: String? = null,
+    maximumInstanceCount: Int? = null,
+    maximumPermitCount: Int = 8
+): BinaryDataPool = invoke(location, maximumInstanceCount, maximumPermitCount)
