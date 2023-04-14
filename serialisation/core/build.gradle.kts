2b2de37a5459dccb7600df3ab001d39884fc2943
Izzy <undermybrella@abimon.org>
2023-04-14 15:04:32 +1000

    Add case insensitivity options for string (de)serialisation

--- serialisation/core/build.gradle.kts
+++ serialisation/core/build.gradle.kts
@@ -1,6 +1,6 @@
 apply(plugin = "org.jetbrains.kotlin.multiplatform")
 
-version = "2.0.0-alpha"
+version = "2.1.0-alpha"
 
 multiplatform {
     /* Targets configuration omitted. 
--- serialisation/core/src/commonMain/kotlin/dev/brella/kornea/serialisation/core/common/KorneaStringEnum.kt
+++ serialisation/core/src/commonMain/kotlin/dev/brella/kornea/serialisation/core/common/KorneaStringEnum.kt
@@ -11,20 +11,28 @@ public interface KorneaStringEnum {
     public val type: String
 }
 
+public enum class EnumCaseSensitivity(public val transform: (String) -> String) {
+    AS_IS(String::toString),
+    LOWER_CASE(String::lowercase),
+    UPPER_CASE(String::uppercase)
+}
+
 public open class KorneaStringEnumSerialiser<T : KorneaStringEnum>(
     public val values: Lazy<Map<String, T>>,
     public val serialName: String,
+    public val deserialisingCaseSensitivity: EnumCaseSensitivity = EnumCaseSensitivity.AS_IS,
+    public val serialisingCaseSensitivity: EnumCaseSensitivity = EnumCaseSensitivity.AS_IS,
     public val default: (String) -> T,
 ) : KSerializer<T> {
     override val descriptor: SerialDescriptor =
         PrimitiveSerialDescriptor(serialName, PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: T) {
-        encoder.encodeString(value.type)
+        encoder.encodeString(serialisingCaseSensitivity.transform(value.type))
     }
 
     override fun deserialize(decoder: Decoder): T {
-        val type = decoder.decodeString()
+        val type = deserialisingCaseSensitivity.transform(decoder.decodeString())
 
         return values.value[type] ?: default(type)
     }
@@ -33,6 +41,8 @@ public open class KorneaStringEnumSerialiser<T : KorneaStringEnum>(
 public open class KorneaEnumLikeSerialiser<T>(
     public val values: Map<String, T>,
     public val serialName: String,
+    public val deserialisingCaseSensitivity: EnumCaseSensitivity = EnumCaseSensitivity.AS_IS,
+    public val serialisingCaseSensitivity: EnumCaseSensitivity = EnumCaseSensitivity.AS_IS,
     public val typeToDefault: (String) -> T,
     public val unknownToType: (T) -> String,
 ) : KSerializer<T> {
@@ -40,12 +50,13 @@ public open class KorneaEnumLikeSerialiser<T>(
         PrimitiveSerialDescriptor(serialName, PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: T) {
-        encoder.encodeString((values.firstNotNullOfOrNull { (k, v) -> if (v == value) k else null }
-            ?: unknownToType(value)))
+        encoder.encodeString(serialisingCaseSensitivity.transform(
+            (values.firstNotNullOfOrNull { (k, v) -> if (v == value) k else null } ?: unknownToType(value))
+        ))
     }
 
     override fun deserialize(decoder: Decoder): T {
-        val type = decoder.decodeString()
+        val type = deserialisingCaseSensitivity.transform(decoder.decodeString())
 
         return values[type] ?: typeToDefault(type)
     }
